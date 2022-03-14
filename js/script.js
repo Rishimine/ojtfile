cd01c44cbe9bf5219457b3e25664af09dfe024ba
arakawahokuto <4869bigdipper@gmail.com>
2022-03-14 18:05:53 +0900

    no message

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+$(function(){
+
+    console.log("test");
+    // $(".main_img").fadeOut(3000);
+    
+    // $(".main_img").bgSwitcher({
+    //     images: ["./img/visual_1_pc.png", "./img/visual_1_pc.png", "./img/visual_1_pc.png"],
+    //     interval: 6000,
+    //     loop: true,
+    //     shuffle: true,
+    //     effect: "drop",
+    //     duration: 750,
+    //     easing: "swing"
+    // });
+})
