51b7cff8963c06321cdc4536532ed4883969ba94
伊志嶺里菜 <>
2022-03-15 16:02:18 +0900

    コミット

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php get_header();?>
 <main>
         <div class="main_visual">
-            <img src="./img/logo.png" alt="" class="logo_img">
+            <img src="<?php echo get_template_directory_uri();?>/img/logo.png" alt="" class="logo_img">
             <div class="swiper-container">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide">
                         <div class="slide-img">
-                            <img src="./img/visual_1_pc.png" alt="スライダー画像1" class="pc">
-                            <img src="./img/visual_1_sp.png" alt="スライダー画像1" class="sp">
+                            <img src="<?php echo get_template_directory_uri();?>/img/visual_1_pc.png" alt="スライダー画像1" class="pc">
+                            <img src="<?php echo get_template_directory_uri();?>/img/visual_1_sp.png" alt="スライダー画像1" class="sp">
                         </div>
                     </div>
                     </div>
             </div>
-            <img src="./img/visual_text_pc.png" alt="" class="text_img">
+            <img src="<?php echo get_template_directory_uri();?>/img/visual_text_pc.png" alt="" class="text_img">
         </div>
         <div class="visual_text">
             <p class="inner_text">
@@ -21,26 +21,32 @@
             </p>
         </div>
         <!-- メインループ -->
-        <?php 
-        if (condition)?>
+        <?php
+    if(have_posts()):
+    while (have_posts()):
+      the_post();
+    ?>
 
         <h1 id="title">Latest Articles</h1>
         <section class="text-box">
             <div class="content">
-                <img src="./img/post_img_1.png" alt="">
-                <time>2022 / 03 /14 </time>
-                <p>内容</p>
-                <div class="btn"><a href="">READ MORE</a></div>
+                <img src="<?php echo get_template_directory_uri();?>/img/post_img_1.png" alt="">
+                <time><?php echo get_the_date();?></time>
+                <p><?php the_title();?></p>
+                <span><?php the_ID();?></span>
+                <div class="btn">
+                <a href="<?php the_permalink(); ?>">
+          <?php the_post_thumbnail(); ?>READ MORE</a>
             </div>
             <div class="content">
-                <img src="./img/post_img_2.png" alt="">
+                <img src="<?php echo get_template_directory_uri();?>/img/post_img_2.png" alt="">
                 <time>2022 / 03 /14 </time>
                 <p>内容</p>
                 <div class="btn"><a href="">READ MORE</a></div>
 
             </div>
             <div class="content">
-                <img src="./img/post_img_3.png" alt="">
+                <img src="<?php echo get_template_directory_uri();?>/img/post_img_3.png" alt="">
                 <time>2022 / 03 /14 </time>
                 <p>内容</p>
                 <div class="btn"><a href="">READ MORE</a></div>
@@ -49,20 +55,20 @@
         </section>
         <section class="text-box">
             <div class="content">
-                <img src="./img/post_img_4.png" alt="">
+                <img src="<?php echo get_template_directory_uri();?>/img/post_img_4.png" alt="">
                 <time>2022 / 03 /14 </time>
                 <p>内容</p>
                 <div class="btn"><a href="">READ MORE</a></div>
             </div>
             <div class="content">
-                <img src="./img/post_img_5.png" alt="">
+                <img src="<?php echo get_template_directory_uri();?>/img/post_img_5.png" alt="">
                 <time>2022 / 03 /14 </time>
                 <p>内容</p>
                 <div class="btn"><a href="">READ MORE</a></div>
 
             </div>
             <div class="content">
-                <img src="./img/post_img_6.png" alt="">
+                <img src="<?php echo get_template_directory_uri();?>/img/post_img_6.png" alt="">
                 <time>2022 / 03 /14 </time>
                 <p>内容</p>
                 <div class="btn"><a href="">READ MORE</a></div>
@@ -76,5 +82,6 @@
         <section>
             <p>表示する記事がありません。</p>
         </section>
+        <?php endif; ?>
     </main>
 <?php get_footer();?>
