f7e9a12c8530fd50a1e54b1e40799fbf3d277d71
伊志嶺里菜 <>
2022-03-15 14:46:43 +0900

    php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+        <p class="copyright">Copyright &copy; Shinohara. ALL RIGHTS RESERVED</p>
+    </footer>
+    <?php wp_footer();?>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- <script src="./assets/js/jquery.bgswitcher.js"></script> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/js/swiper.min.js"></script>
+    <script src="./js/script.js"></script>
+</body>
+
+</html>
